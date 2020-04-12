1103e4bf0d754b597b4cee10c946e2cb50bf5082
pablofc <pablo.fernando.cargnelutti@gmail.com>
2020-04-12 13:05:15 -0400

    Adding parallel crawling.

--- crawling/crawler.js.py
+++ crawling/crawler.js.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from concurrent.futures.process import ProcessPoolExecutor
 
 import requests_html
 import urllib3
@@ -15,6 +16,30 @@ def readUrls(file_name = 'Alexa_topsites.txt'):
     file.close()
     return ['http://www.' + url for url in urls]
 
+def process_url(url, folder_name):
+    session = requests_html.HTMLSession(verify=False)
+    try:
+        print('Crawling %s' % url)
+        r = session.get(url, verify=False)
+        r.html.render(retries=4, sleep=1, wait=2)
+        scripts = r.html.find('script')
+        r.close()
+    except Exception as e:
+        print(e)
+        print("Error with %s, moving to the next url." % url)
+        session.close()
+
+    i = 1
+    for script in scripts:
+        if 'type' in script.attrs and script.attrs['type'] == 'text/javascript' and 'src' not in script.attrs:
+            name = '%s/%s/%s-%d.js' % (os.getcwd(), folder_name, url[7:], i)
+            file = open(name, 'w')
+            file.write(script.text)
+            file.flush()
+            file.close()
+            i += 1
+    session.close()
+
 
 if __name__ == '__main__':
     file_name = sys.argv[1] if len(sys.argv) > 1 else 'Alexa_topsites.txt'
@@ -25,26 +50,8 @@ if __name__ == '__main__':
     urls = readUrls(file_name)
     total = len(urls)
     current = 0
-    for url in urls:
-        current += 1
-        session = requests_html.HTMLSession(verify=False)
-        try:
-            print('Crawling %s (%d / %d)' % (url, current, total))
-            r = session.get(url, verify=False)
-            r.html.render(retries=4, sleep=1, wait=1)
-            scripts = r.html.find('script')
-            r.close()
-        except:
-            session.close()
-            continue
-        i = 1
-        for script in scripts:
-            if 'type' in script.attrs and script.attrs['type'] == 'text/javascript' and 'src' not in script.attrs:
-                name = '%s/%s/%s-%d.js' % (os.getcwd(), folder_name, url[7:], i)
-                file = open(name, 'w')
-                file.write(script.text)
-                file.flush()
-                file.close()
-                i += 1
 
-        session.close()
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        _futures = [executor.submit(process_url, url, folder_name) for url in urls]
+        results = []
+        executor.shutdown(wait=True)
